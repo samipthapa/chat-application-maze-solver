352295e12417abc7d86d12204a7702ef580875dc
Samip Bikram Thapa <samipthapa99@gmail.com>
2025-02-27 17:22:38 +0545

    Project Setup

---
+++ src/main/java/com/samip/chatmaze/ChatApplicationWithMazeSolver.java
@@ -0,0 +1,13 @@
+package com.samip.chatmaze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplicationWithMazeSolver {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplicationWithMazeSolver.class, args);
+	}
+
+}
---
+++ src/test/java/com/samip/chatmaze/ChatApplicationWithMazeSolverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samip.chatmaze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationWithMazeSolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
